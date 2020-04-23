9edaa0d28c4dbe8ab7c902ece7f8821af46d9fce
Wesley <wesaboo@gmail.com>
2020-04-22 21:03:00 -0700

    still working on passgen

--- passgen.js
+++ passgen.js
@@ -21,5 +21,71 @@ function getRandomSymbol() {
     return symbols[Math.floor(Math.random()*symbols.length)];
 }
 
+function sliderChange(val) {
+    document.getElementById('sliderVal').innerHTML = val;
+}
 
-console.log(getRandomSymbol());
+var randomFunc = {
+    lower: getRandomLower,
+    upper: getRandomUpper,
+    number: getRandomNumber,
+    symbol: getRandomSymbol
+};
+
+var password = document.getElementById('password'); //Input Box
+var sliderVal = document.getElementById('sliderVal'); //Value from Slider
+var upper = document.getElementById('upper'); //Uppercase Letter Checkbox
+var lower = document.getElementById('lower'); //Lowercase Letter Checkbox
+var number = document.getElementById('number'); //Number Checkbox
+var symbol = document.getElementById('symbol'); //Symbol Checkbox
+var generate = document.getElementById('generate'); //Generate Button
+var clipboard = document.getElementById('clipboard'); //Copy to Clipboard Button
+
+//On Click
+generate.addEventListener('click', () => {
+    var length = +sliderVal.Value;
+    var truUpper = upper.checked;
+    var truLower = lower.checked;
+    var truNumber = number.checked;
+    var truSymbol = symbol.checked;
+
+    password.innerText= generatePassword(
+        truUpper, 
+        truLower,
+        truNumber,
+        truSymbol, 
+        length
+    );
+});
+
+//Password Generator Script
+function generatePassword(truUpper,truLower,truNumber,truSymbol,length) {
+    var generatedPassword = '';
+    var typesCount = truUpper+truLower+truNumber+truSymbol;
+
+    console.log('typesCount: ', typesCount);
+
+    var Array = [{truUpper},{truLower},{truNumber},{truSymbol}].filter
+    (
+        item => Object.values(item)[0]
+    );
+    
+    console.log('Array: ', Array);
+
+    if(typesCount === 0){
+        return 'Please select password specifications';
+    }
+
+    for(i=0; i<length; i+= typesCount){
+        Array.forEach(type => {
+            var funcName = Object.keys(type)[0];
+            console.log('funcName: ', funcName);
+           
+            generatedPassword += randomFunc[funcName]();
+        });
+    }
+
+    console.log(generatedPassword);
+
+
+};
