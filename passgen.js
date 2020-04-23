0173142de51f0040314b6e812d7b3d02387d71c5
Wesley <wesaboo@gmail.com>
2020-04-22 19:44:47 -0700

    coded random number and symbol

--- passgen.js
+++ passgen.js
@@ -1,12 +1,25 @@
 //CharSet- http://net-comber.com/charset.html
 
+// Random Lower Case Letters
 function getRandomLower() {
     return String.fromCharCode((Math.floor(Math.random()*26)+97));
 }
 
+// Random Upper Case Letters
 function getRandomUpper() {
     return String.fromCharCode((Math.floor(Math.random()*26)+65));
 }
 
+//Random Numbers
+function getRandomNumber() {
+    return String.fromCharCode((Math.floor(Math.random()*10)+48));
+}
 
-console.log(getRandomUpper());
+//Random Symbols
+function getRandomSymbol() {
+    var symbols = '!@#$%^&*(){}[]=<>/,.\|+_-=`~';
+    return symbols[0];
+}
+
+
+console.log(getRandomSymbol());
