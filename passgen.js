337f74355dabf99682f4e4e6ba168c9b384dbe5d
Wesley <wesaboo@gmail.com>
2020-05-18 03:40:13 -0700

    attempt to use .match function, unable to utilize recursion

--- passgen.js
+++ passgen.js
@@ -1,10 +1,10 @@
 // Variables
 
 const passwordBox = document.querySelector("#password");
+const randomSymbol = [33,47];
 const randomNum =[48,57];
 const upperCase = [65,90];
 const lowerCase = [97,122];
-const randomSymbol = [33,47];
 let empty = "Please select at least one type of characters!"
 
 // Slider
@@ -19,8 +19,10 @@ clipboard.onclick = function () {
 }
 
 // Event listener for the generate button
-document.querySelector("#generate").addEventListener('click', function generatePass() {
-  const length = document.querySelector("#sliderVal").value;
+document.querySelector("#generate").addEventListener('click', generatePass)
+
+function generatePass() {
+  const passLength = document.querySelector("#sliderVal").value;
   const upper = document.querySelector("#upper").checked;
   const lower = document.querySelector("#lower").checked;
   const numbers = document.querySelector("#number").checked;
@@ -34,8 +36,8 @@ document.querySelector("#generate").addEventListener('click', function generateP
     alert(empty)
   }
 
-  if(upper===true){
-    for(let i=upperCase[0]; i<= upperCase[1]; i++){
+  if(symbols===true){
+    for(let i=randomSymbol[0]; i<= randomSymbol[1]; i++){
       ranSelector.push(i);
     }
   }
@@ -44,8 +46,8 @@ document.querySelector("#generate").addEventListener('click', function generateP
       ranSelector.push(i);
     }
   }
-  if(symbols===true){
-    for(let i=randomSymbol[0]; i<= randomSymbol[1]; i++){
+  if(upper===true){
+    for(let i=upperCase[0]; i<= upperCase[1]; i++){
       ranSelector.push(i);
     }
   }
@@ -54,22 +56,31 @@ document.querySelector("#generate").addEventListener('click', function generateP
       ranSelector.push(i);
     }
   }
-  
+  //console.log(ranSelector)
   // Generate and push password to input box
-  for(let i = 0; i< length; i++){
+  for(let i = 0; i< passLength; i++){
     password.push(String.fromCharCode(ranSelector[Math.floor(Math.random()*ranSelector.length)]))
   }
   let generated = password.textContent = password.join("");
+  
+  // Utilizing .match to ensure password contains the characters selected
   if(symbols===true){
     let regex = /[^\w\s]/g;
     let found = generated.match(regex);
-      if(found===null || 0){
-        console.log("No Symbols Found");
+      if(found === null){
+        console.log("no symbols");
         generatePass();
-      } else {
-        passwordBox.value = generated;
+      } 
+  }
+  if(numbers===true){
+    let regex = /[0-9]/g;
+    let found = generated.match(regex);
+      if(found === null){
+        console.log("no numbers");
+        generatePass();
       }
   }
+
   //console.log(generated)
-  //passwordBox.value = generated;
-})
+  passwordBox.value = generated;
+}
